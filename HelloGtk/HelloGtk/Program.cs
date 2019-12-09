fab4028e2427e77d7844fe39541fcde4c09b0132
JingYu Ning <54fning@gmail.com>
2019-12-09 18:57:41 +0800

    initial proj

---
+++ HelloGtk/HelloGtk/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloGtk
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
